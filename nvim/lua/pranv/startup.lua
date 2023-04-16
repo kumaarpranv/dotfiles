9325eaa47db8019d41bb3bcd1fd31322a27eec61
pranav kumar pulusu <kumaarpranv@gmail.com>
2023-04-16 17:53:28 +0530

    bufenter limit removed

--- nvim/lua/pranv/startup.lua
+++ nvim/lua/pranv/startup.lua
@@ -7,24 +7,24 @@ end
 
 local startupGrp = vim.api.nvim_create_augroup("startup", { clear = true })
 
-vim.api.nvim_create_autocmd("BufEnter", {
-  -- command = "silent! lua vim.highlight.on_yank()",
-  callback = function()
-    local curbufnr = vim.api.nvim_get_current_buf()
-    local buflist = vim.api.nvim_list_bufs()
-    for i, bufnr in ipairs(buflist) do
-      if #(buflist)-(i+1) <= 4 then
-        break
-      end
-      if vim.api.nvim_buf_is_loaded(bufnr) and vim.bo[bufnr].buflisted and bufnr ~= curbufnr and (vim.fn.getbufvar(bufnr, 'bufpersist') ~= 1) then -- nvim_buf_is_loaded(bufnr) and vim.api.nvim_buf_is_valid(v)
-        vim.api.nvim_buf_delete(bufnr, {})
-        -- vim.cmd('bd ' .. tostring(bufnr))
-      end
-    end
-  end,
-  group = startupGrp,
-})
-
+-- vim.api.nvim_create_autocmd("BufEnter", {
+--   -- command = "silent! lua vim.highlight.on_yank()",
+--   callback = function()
+--     local curbufnr = vim.api.nvim_get_current_buf()
+--     local buflist = vim.api.nvim_list_bufs()
+--     for i, bufnr in ipairs(buflist) do
+--       if #(buflist)-(i+1) <= 4 then
+--         break
+--       end
+--       if vim.api.nvim_buf_is_loaded(bufnr) and vim.bo[bufnr].buflisted and bufnr ~= curbufnr and (vim.fn.getbufvar(bufnr, 'bufpersist') ~= 1) then -- nvim_buf_is_loaded(bufnr) and vim.api.nvim_buf_is_valid(v)
+--         vim.api.nvim_buf_delete(bufnr, {})
+--         -- vim.cmd('bd ' .. tostring(bufnr))
+--       end
+--     end
+--   end,
+--   group = startupGrp,
+-- })
+--
 require('pranv.utils.tabby')
 require('pranv.lsp.lsp')
 require('pranv.lsp.treesitter')
