8b8793f2b667be385e7930063d15ba423bcfb397
pranav kumar pulusu <kumaarpranv@gmail.com>
2023-04-24 01:27:52 +0530

    adding transparent background

--- nvim/after/plugin/utils/transparent.lua
+++ nvim/after/plugin/utils/transparent.lua
@@ -2,20 +2,16 @@ local ok, transparent = pcall(require, "transparent")
 if not ok then
   return
 end
---
--- transparent.setup({
---   enable = true, -- boolean: enable transparent
---   extra_groups = { -- table/string: additional groups that should be cleared
---     -- In particular, when you set it to 'all', that means all available groups
---
---     -- example of akinsho/nvim-bufferline.lua
---     "BufferLineTabClose",
---     "BufferlineBufferSelected",
---     "BufferLineFill",
---     "BufferLineBackground",
---     "BufferLineSeparator",
---     "BufferLineIndicatorSelected",
---   },
---   exclude = {}, -- table: groups you don't want to clear
---   ignore_linked_group = true, -- boolean: don't clear a group that links to another group
--- })
+
+transparent.setup({
+  groups = { -- table: default groups
+    'Normal', 'NormalNC', 'Comment', 'Constant', 'Special', 'Identifier',
+    'Statement', 'PreProc', 'Type', 'Underlined', 'Todo', 'String', 'Function',
+    'Conditional', 'Repeat', 'Operator', 'Structure', 'LineNr', 'NonText',
+    'SignColumn', 'CursorLineNr', 'EndOfBuffer',
+  },
+  extra_groups = {}, -- table: additional groups that should be cleared
+  exclude_groups = {}, -- table: groups you don't want to clear
+})
+
+vim.keymap.set("n", "<leader>cs", "<cmd>TransparentToggle<CR>")
