b39e180946898e6cfcdafea46899741e539b64f7
pranav kumar pulusu <kumaarpranv@gmail.com>
2023-04-24 17:36:18 +0530

    transparent global var

--- nvim/after/plugin/utils/transparent.lua
+++ nvim/after/plugin/utils/transparent.lua
@@ -14,4 +14,5 @@ transparent.setup({
   exclude_groups = {}, -- table: groups you don't want to clear
 })
 
+vim.g.transparent_enabled = true
 vim.keymap.set("n", "<leader>cs", "<cmd>TransparentToggle<CR>")
