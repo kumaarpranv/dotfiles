e3d3e0b298acc10ff6b0f88fb5cd836c4300f9c4
pranav kumar pulusu <kumaarpranv@gmail.com>
2023-04-30 22:49:38 +0530

    nightfox

--- nvim/after/plugin/color.lua
+++ nvim/after/plugin/color.lua
@@ -1,5 +1,21 @@
 --vim.g.tokyonight_transparent_sidebar = true
 --vim.g.tokyonight_transparent = true
 vim.opt.background = "dark"
+local ok, nightfox = pcall(require, "nightfox")
+if (not ok) then
+  return
+end
 
-vim.cmd.colorscheme("kanagawa")
+nightfox.setup({
+  options = {
+    styles = {
+      comments = "italic",
+      keywords = "bold",
+      types = "bold",
+      conditionals = "bold",
+      functions = "bold",
+      constants = "bold",
+    }
+  }
+})
+vim.cmd.colorscheme("nightfox")
